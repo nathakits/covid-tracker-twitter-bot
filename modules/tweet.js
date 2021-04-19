a968e5139794840e0e1d2c53427ad6405aba0de5
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-04-20 00:36:52 +0700

    moved twitter and csv parsing functions as modules

---
+++ modules/tweet.js
@@ -0,0 +1,31 @@
+const Twitter = require('twitter-lite');
+
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+const tweetPost = async (data) => {
+  await client.post("statuses/update", {
+    status: data,
+  })
+}
+
+const tweetThread = async (thread) => {
+  let lastTweetID = "";
+  for (const status of thread) {
+    const tweet = await client.post("statuses/update", {
+      status: status,
+      in_reply_to_status_id: lastTweetID,
+      auto_populate_reply_metadata: true
+    });
+    lastTweetID = tweet.id_str;
+  }
+}
+
+module.exports = {
+  tweetPost,
+  tweetThread
+}
