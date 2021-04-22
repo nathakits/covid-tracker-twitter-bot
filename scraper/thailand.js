671659fcf0151bd2f609b7abba7a4e0fdd2866ff
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-04-23 00:19:27 +0700

    update thailand scraper - update pdf URL to include dynamic month - add new regex for scraping date - update scraper to better handle error - add source_url to object and write to json

--- scraper/thailand.js
+++ scraper/thailand.js
@@ -1,30 +1,41 @@
 const fs = require('fs');
 const crawler = require('crawler-request');
+const csv = require('csv');
+const parser = csv.parse();
 const time = require('../modules/time')
-const { scrapePDF } = require('../modules/util')
+const { scrapePDF2JSON } = require('../modules/util')
 
 // vars
-let pdfUrl = `https://ddc.moph.go.th/uploads/ckeditor2//files/Daily%20report%202021-04-${time.date}.pdf`
+let pdfUrl = `https://ddc.moph.go.th/uploads/ckeditor2//files/Daily%20report%202021-${time.formattedMonth}-${21}.pdf`
 let regexArray = [
-  /ผู้ที่ได้รับวัคซีนสะสม (.{1,100}) ทั้งหมด (?<totalVaccinations>.{1,10}) โดส/,
-  /ผู้ได้รับวัคซีนเข็มที่ 1 (.{1,3})นวน (?<singleDose>.{1,10}) ราย/,
-  /\(ได้รับวัคซีน 2 เข็ม\) (.{1,3})นวน (?<twoDoses>.{1,10}) ราย/
+  /\( ข้อมูล ณ วันที่ (?<date>.{1,30}) เวลา (?<time>.{1,10}) น. \)/,
+  /ผู้ที่ได้รับวัคซีนสะสม (.{1,100}) ทั้งหมด (?<total_vaccinations>.{1,10}) โดส/,
+  /ผู้ได้รับวัคซีนเข็มที่ 1 (.{1,3})นวน (?<people_vaccinated>.{1,10}) ราย/,
+  /\(ได้รับวัคซีน 2 เข็ม\) (.{1,3})นวน (?<people_fully_vaccinated>.{1,10}) ราย/
 ]
 
 crawler(pdfUrl)
   .then(res => {
-    scrapePDF(res, regexArray).then((data) => {
+    scrapePDF2JSON(res, regexArray).then((data) => {
+      data.source_url = pdfUrl
       let stringified = JSON.stringify(data, null, 2)
       console.log(stringified);
+      // write to json file
       fs.writeFileSync(`./data/vaccinations.json`, stringified)
+      // update csv file TODO
+      // fs.readFile('./data/Thailand.csv', (err, fileData) => {
+      //   if (fileData) {
+      //     let records = parser(fileData, {
+      //       columns: true
+      //     });
+      //     console.log(records);
+      //   }
+      // })
     }).catch(err => {
-      console.log(`Scrape error: PDF URL doesn't exist`);
-      console.log(err);
+      if (res.status === 404) {
+        console.log(res);
+      }
+      console.error(err);
       process.exit(1)
     })
   })
-  .catch(err => {
-    console.log(`Crawler error`)
-    console.log(err);
-    process.exit(1)
-  });
