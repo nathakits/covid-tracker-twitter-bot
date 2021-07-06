bbe84a8cd45b8a25b24db360dfb7b88b434f1dd6
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-07-06 19:57:49 +0700

    add new script to read csv and write json

---
+++ modules/csv2json.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path')
+const csv = require('csvtojson')
+const filePath = path.join(__dirname, '../data/Thailand.csv');
+const genJSON = async () => {
+  const data = await csv().fromFile(filePath)
+  let stringify = JSON.stringify(data, null, 2)
+  fs.writeFile('data/Thailand.json', stringify, (err) => {
+    if (err) throw err
+    console.log(`Status: Complete`);
+  });
+}
+
+genJSON()
