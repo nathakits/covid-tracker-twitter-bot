f5d1e68a836442bc6f8f97425a26c41a2feeac79
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-07-26 19:36:54 +0700

    update csvtojson script - remove whitespaces from json data

--- modules/csv2json.js
+++ modules/csv2json.js
@@ -4,7 +4,7 @@ const csv = require('csvtojson')
 const filePath = path.join(__dirname, '../data/Thailand.csv');
 const genJSON = async () => {
   const data = await csv().fromFile(filePath)
-  let stringify = JSON.stringify(data, null, 2)
+  let stringify = JSON.stringify(data)
   fs.writeFile('data/Thailand.json', stringify, (err) => {
     if (err) throw err
     console.log(`Status: Complete`);
